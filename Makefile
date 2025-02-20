df7610969b9fe51d8eb5c0a40e1f96a4f892ed81
Sebastian Urchs <sebastian.urchs@gmail.com>
2025-02-20 15:20:44 -0500

    Add a script to map CogAtlas to SNOMED

--- Makefile
+++ Makefile
@@ -5,8 +5,11 @@
 fetch:
 	python src/fetch_data.py
 
-process:
+process: 
 	python src/process_data.py
+
+map: data_dictionary.json
+	python src/map_data.py
 
 loop:
 	for i in {1..5}; do echo "Iteration $$i"; done
---
+++ src/map_data.py
@@ -0,0 +1,34 @@
+import json
+
+import pandas as pd
+
+
+def make_map(input_file, table_file, output_file):
+    
+    
+    my_map = {}
+    
+    with open(input_file, 'r') as f:
+        dictionary = json.load(f)
+        
+    table = pd.read_csv(table_file, sep="\t")
+        
+    for key, value in dictionary.items():
+        if "Annotations" in value and "IsPartOf" in value["Annotations"]:
+            snomed_id = value["Annotations"]["IsPartOf"]["TermURL"]
+        else:
+            continue
+        
+        # Look up the cogAtlas ID for the tool name and then map it to the new SNOMED ID
+        cogatlas_id = table[key].values[0]
+        my_map[cogatlas_id] = snomed_id
+        
+    with open(output_file, 'w') as f:
+        json.dump(my_map, f, indent=4)
+    
+
+if __name__ == "__main__":
+    DICTIONARY_FILE = "data_dictionary.json"
+    TABLE_FILE= "assessments.tsv"
+    OUTPUT_FILE = "map.json"
+    make_map(DICTIONARY_FILE, TABLE_FILE, OUTPUT_FILE)
